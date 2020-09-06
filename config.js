b1cf856f6266dbfceff265f5a87f19d4ee9f81b1
rickychhoukdean <chhoukdean184@msn.com>
2020-09-06 17:26:29 -0400

    Initial commit from https://github.com/anubhavsrivastava/gatsby-starter-spectral

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteTitle: 'Ricky Chhoukdean Portfolio', // <title>
+  manifestName: 'Spectral',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-spectral/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Ricky Chhoukdean',
+  subHeading: 'Full Stack Developer',
+  socialLinks: [
+    {
+      style: 'brands',
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/rickychhoukdean',
+    },
+    {
+      style: 'brands',
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/chhoukdean/',
+    },
+    {
+      style: 'solid',
+      icon: 'fa-envelope',
+      name: 'Email',
+      url: 'mailto:ricky@chhoukdean.ca',
+    },
+  ],
+};
